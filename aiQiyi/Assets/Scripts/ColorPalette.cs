96d870f4be22c80330ff8a621a916d53b6193ec5
zhangtao04 <zhangtao04@qiyi.com>
2025-08-27 11:20:43 +0800

    *颜色光的设置

--- aiQiyi/Assets/Scripts/ColorPalette.cs
+++ aiQiyi/Assets/Scripts/ColorPalette.cs
@@ -4,4 +4,6 @@ using UnityEngine;
 public class ColorPalette : ScriptableObject
 {
     public Color[] colors; // 存储颜色的数组
+    public float[] lightIntensities; // 灯光强度数组，与颜色数组一一对应
+    public Color[] emissionColors;   // 自发光颜色数组，与颜色数组一一对应
 }
